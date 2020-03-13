cc20c4c2f0b14dca0efae5256a65227cfe4bae08
Antony <holshouserantony@yahoo.com>
2020-03-12 22:48:52 -0700

    Version 1

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -23,18 +23,89 @@ public class MaxHeap implements Heap {
     // add an item to the heap
     public boolean add(Integer item) {
         // homework
-        return false;
+        if (size == data.length) return false;
+
+        data[size] = item;
+        int index = size;
+        size++;
+
+        while (index != 0 && data[index] > data[(index-1/2)]) {
+            Integer temp = data[index];
+            data[index] = data[(index-1)/2];
+            data[(index-1)/2] = temp;
+            index = (index-1)/2;
+        }
+
+        return true;
     }
 
     // return the max item in the heap
     public Integer get() {
         // homework
-        return null;
+        return data[0];
     }
 
     // remove the root item
     public Integer pop() {
         // homework
-        return null;
+
+        Integer toReturn = data[0];
+
+        size--;
+        data[0] = data[size];
+        int index = 0;
+
+        while ((index*2)+2 < size && (data[index] < data[(index*2)+1] || data[index] < data[(index*2)+2])) {
+            if (data[(index*2)+1] > data[(index*2)+2]) {
+                Integer temp = data[index];
+                data[index] = data[(index*2)+2];
+                data[(index*2)+2] = temp;
+                index = (index*2)+2;
+            } else {
+                Integer temp = data[index];
+                data[index] = data[(index*2)+1];
+                data[(index*2)+1] = temp;
+                index = (index*2)+1;
+            }
+        }
+
+        if ((index*2)+1 < size) {
+            Integer temp = data[index];
+            data[index] = data[(index*2)+2];
+            data[(index*2)+2] = temp;
+            index = (index*2)+2;
+        }
+
+        //                0
+        //               5 4
+        //             3 2 1
+
+        /**
+        while ((data[index] < data[(index*2)] || data[index] < data[(index*2)+1]) && index*2 < size) {
+
+            if (index*2 >= size) {
+                break;
+            }
+
+            if ((index*2)+1 >= size && dat) {
+
+            }
+
+            if (data[index*2] < data[(index*2)+1]) {
+                Integer temp = data[index];
+                data[index] = data[(index*2)+1];
+                data[(index*2)+1] = temp;
+                index = (index*2)+1;
+            } else {
+                Integer temp = data[index];
+                data[index] = data[index*2];
+                data[index*2] = temp;
+                index = index*2;
+            }
+        }
+         */
+
+
+        return toReturn;
     }
 }
